81c62580ba4e4c2e796d5b00b2874a9446b8fb62
xxdexx233 <1813692108@qq.com>
2026-03-16 22:01:45 +0800

    lab4 done

---
+++ Project-Semester2/InputOutput.sh
@@ -0,0 +1,3 @@
+echo Please enter a value:
+read number
+echo You entered: $number
---
+++ Project-Semester2/stats.sh
@@ -0,0 +1,8 @@
+echo The current time and date is:
+date
+echo
+echo The number of files in my system are:
+ls -l | wc -l
+echo
+echo Your current working directory is:
+pwd
